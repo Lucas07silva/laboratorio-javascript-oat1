acf2d35827ca43ab2deb15d85349a49b5e4c1a3d
Diogo Perez <diogoperezmelo@gmail.com>
2026-09-15 22:12:32 -0300

    Update questão1

--- src/questao01-contagem-regressiva.js
+++ src/questao01-contagem-regressiva.js
@@ -1,1 +1,26 @@
+function calcularContagemRegressiva(dataFutura) {
+    const agora = new Date().getTime();
+    const futuro = new Date(dataFutura).getTime();
+    
+    const diferenca = futuro-agora;
 
+    const segundo = 1000;
+    const minuto = segundo*60;
+    const hora = minuto*60;
+    const dia = hora*24;
+
+    const dias = Math.floor(diferenca/dia);
+    const horas = Math.floor((diferenca%dia)/hora);
+    const minutos = Math.floor((diferenca%)/minuto);
+    const segundos = Math.floor((diferenca%minuto)/segundo);
+
+    return {
+        dias: dias,
+        horas: horas,
+        minutos: minutos,
+        segundos: segundos,
+        texto:`falta ${dias} dias, ${horas}h, ${minutos}m e ${segundos}`
+    }
+}
+const resultado = calcularContagemRegressiva("2027-01-01T00:00:00");
+console.log(resultado);
