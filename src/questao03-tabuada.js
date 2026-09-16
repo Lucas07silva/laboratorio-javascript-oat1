31140a95a846be5e2d2ef9996192863a5407855e
Diogo Perez <diogoperezmelo@gmail.com>
2026-09-15 22:50:17 -0300

    Update questao03-tabuada.js

--- src/questao03-tabuada.js
+++ src/questao03-tabuada.js
@@ -1,1 +1,11 @@
+function gerarTabuada(numero, limite = 10){
+    const tabuada = []
 
+    for (let i = 1; i <= limite; i++){
+        const resultado = numero * i;
+        tabuada.push(`${numero} x ${i} = ${resultado}`); 
+    }
+
+    return tabuada;
+}
+console.log(gerarTabuada(7, 5));
